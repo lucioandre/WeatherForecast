238addb4921dfb20effcac2cc15f8830078ce7ee
Lucio <V-FonsL-Ave@ae.com>
2016-08-15 13:20:49 -0300

    First Commit

---
+++ TheWeatherForecast/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  TheWeatherForecast
+//
+//  Created by Avenue Brazil on 8/15/16.
+//  Copyright © 2016 Lucio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ TheWeatherForecast/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TheWeatherForecast
+//
+//  Created by Avenue Brazil on 8/15/16.
+//  Copyright © 2016 Lucio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
